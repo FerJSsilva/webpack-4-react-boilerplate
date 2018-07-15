32c7e29eae4e081d574ba35c7ab5848702693d86
Fernando <development.ferjssilva@gmail.com>
2018-07-15 14:27:56 -0300

    feature: add React to the project
    Adding react as first dependencies. Additional Webpack's configurations is still necessary.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Webpack 4 React Boilerplate</h1>
+            </div>
+        );
+    }
+} 
+
+export default App;
