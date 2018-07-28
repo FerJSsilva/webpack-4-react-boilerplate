551adee4b79099382c05243d6b10a6e267a39166
Fernando <development.ferjssilva@gmail.com>
2018-07-28 16:47:45 -0300

    feature: add analyze option to command scripts
    With this new feature it´s possible to analize the size of the builded bundle for distribution code (dist). Cross env is necessary to set environment variables in any SO

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const enableBundleAnalyzer = process.env.ENABLE_ANALYZER === 'true';
 
 module.exports = merge (common, {
     mode: 'production',
@@ -10,6 +13,10 @@ module.exports = merge (common, {
             root: process.cwd(),
             verbose: true, 
             dry: false
-        })
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: enableBundleAnalyzer === true ? 'static' : 'disabled',
+            openAnalyzer: true,
+          }),
     ],
 });
